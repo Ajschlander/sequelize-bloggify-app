aa89d696a31bcce2b000670a47e21b3819fcb2e4
Ajschlander <Aschlander21@gmail.com>
2020-01-07 10:49:05 -0600

    Setup

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,5 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS bloggify_db;
+CREATE DATABASE bloggify_db;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+DROP DATABASE IF EXISTS test_bloggify_db;
+CREATE DATABASE test_bloggify_db;
