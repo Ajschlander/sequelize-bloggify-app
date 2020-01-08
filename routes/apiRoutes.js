992bec737a789bb815219162be82a63ce54449f5
Ajschlander <Aschlander21@gmail.com>
2020-01-08 10:38:04 -0600

    User can now create a new blog

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -18,9 +18,28 @@ module.exports = {
       });
     });
 
+    // CREATE ROUTE
+    app.post("/blogs", (req, res) => {
+      req.body.blog.body = req.sanitize(req.body.blog.body);
+      db.Blog.create({
+        title: req.body.blog.title,
+        image: req.body.blog.image,
+        body: req.body.blog.body
+      }).then(() => {
+        res.redirect("/blogs");
+      });
+    });
+
     // NEW ROUTE FOR CREATING A NEW BLOG POST
     app.get("/blogs/new", (req, res) => {
       res.render("new");
+    });
+
+    // SHOW ROUTE
+    app.get("/blogs/:id", (req, res) => {
+      db.Blog.find({ where: { id: req.params.id } }).then(foundBlog => {
+        res.render("show", { blog: foundBlog });
+      });
     });
   }
 };
