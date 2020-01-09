2c5f89f77a28530386eb25ae8b27957a35d8337b
Ajschlander <Aschlander21@gmail.com>
2020-01-09 15:21:46 -0600

    Added login functionality

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -11,69 +11,65 @@ module.exports = {
       res.redirect("/signup");
     });
 
-    app.get("/signup", (req, res) => {
-      res.render("signup");
-    });
-
-    // GET ROUTE FOR THE HOME PAGE WITH ALL THE BLOGS IN THE DB
-    app.get("/blogs", (req, res) => {
-      db.Blog.findAll({}).then(blogs => {
-        res.render("index", { blogs: blogs });
-      });
-    });
+    // // // GET ROUTE FOR THE HOME PAGE WITH ALL THE BLOGS IN THE DB
+    // app.get("/blogs", (req, res) => {
+    //   db.Blog.findAll({}).then(blogs => {
+    //     res.render("index", { blogs: blogs });
+    //   });
+    // });
 
     // CREATE ROUTE
-    app.post("/blogs", (req, res) => {
-      req.body.blog.body = req.sanitize(req.body.blog.body);
-      db.Blog.create({
-        title: req.body.blog.title,
-        image: req.body.blog.image,
-        body: req.body.blog.body
-      }).then(() => {
-        res.redirect("/blogs");
-      });
-    });
+    // app.post("/blogs", (req, res) => {
+    //   req.body.blog.body = req.sanitize(req.body.blog.body);
+    //   db.Blog.create({
+    //     title: req.body.blog.title,
+    //     image: req.body.blog.image,
+    //     body: req.body.blog.body
+    //   }).then(() => {
+    //     res.redirect("/blogs");
+    //   });
+    // });
 
     // NEW ROUTE FOR CREATING A NEW BLOG POST
-    app.get("/blogs/new", (req, res) => {
-      res.render("new");
-    });
+    // app.get("/blogs/new", (req, res) => {
+    //   res.render("new");
+    // });
 
     // SHOW ROUTE
-    app.get("/blogs/:id", (req, res) => {
-      db.Blog.findOne({ where: { id: req.params.id } }).then(foundBlog => {
-        res.render("show", { blog: foundBlog });
-      });
-    });
+    // app.get("/blogs/:id", (req, res) => {
+    //   db.Blog.findOne({ where: { id: req.params.id } }).then(foundBlog => {
+    //     res.render("show", { blog: foundBlog });
+    //   });
+    // });
 
     // EDIT ROUTE
-    app.get("/blogs/:id/edit", (req, res) => {
-      db.Blog.findOne({ where: { id: req.params.id } }).then(foundBlog => {
-        res.render("edit", { blog: foundBlog });
-      });
-    });
+    // app.get("/blogs/:id/edit", (req, res) => {
+    //   db.Blog.findOne({ where: { id: req.params.id } }).then(foundBlog => {
+    //     res.render("edit", { blog: foundBlog });
+    //   });
+    // });
 
     // UPDATE ROUTE
-    app.put("/blogs/:id", (req, res) => {
-      req.body.blog.body = req.sanitize(req.body.blog.body);
-      db.Blog.findOne({ where: { id: req.params.id } }).then(updatedBlog => {
-        updatedBlog
-          .update({
-            title: req.body.blog.title,
-            image: req.body.blog.image,
-            body: req.body.blog.body
-          })
-          .then(() => {
-            res.redirect("/blogs");
-          });
-      });
-    });
+    // app.put("/blogs/:id", (req, res) => {
+    //   req.body.blog.body = req.sanitize(req.body.blog.body);
+    //   db.Blog.findOne({ where: { id: req.params.id } }).then(updatedBlog => {
+    //     updatedBlog
+    //       .update({
+    //         title: req.body.blog.title,
+    //         image: req.body.blog.image,
+    //         body: req.body.blog.body
+    //       })
+    //       .then(() => {
+    //         res.redirect("/blogs");
+    //       });
+    //   });
+    // });
 
-    // DESTROY ROUTE
-    app.delete("/blogs/:id", (req, res) => {
-      db.Blog.destroy({ where: { id: req.params.id } }).then(() => {
-        res.redirect("/");
-      });
-    });
+    // // DESTROY ROUTE
+    // app.delete("/blogs/:id", (req, res) => {
+    //   db.Blog.destroy({ where: { id: req.params.id } }).then(() => {
+    //     res.redirect("/");
+    //   });
+    // });
   }
 };
